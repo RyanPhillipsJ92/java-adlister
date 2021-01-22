d9d7f56949f692441c64e5c494b8886679936906
Ryan Phillips <ryan.phillips.j92@gmail.com>
2021-01-22 12:28:24 -0600

    working on accessing data with DAOs

--- src/main/java/BeanTest.java
+++ src/main/java/BeanTest.java
@@ -4,6 +4,7 @@ import models.Employee;
 import models.Quote;
 
 import java.time.Year;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class BeanTest {
@@ -19,17 +20,34 @@ public class BeanTest {
         Author author = new Author();
         author.setFirstName("Ryan");
         author.setLastName("Phillips");
-        author.setIdNo(8675309);
+        author.setIdNo(1L);
 
-        Quote quote= new Quote();
 
-        quote.setContent("Close only matters in horseshoes and hand grenades");
-        quote.setIdNo(8675309);
+        Quote firstQuote= new Quote();
+        firstQuote.setAuthor(author);
+        firstQuote.setContent("Close only matters in horseshoes and hand grenades");
+        firstQuote.setIdNo(1L);
+
+     Quote secondQuote= new Quote();
+     secondQuote.setAuthor(author);
+     secondQuote.setContent("blah blah blah");
+     secondQuote.setIdNo(2L);
 
         System.out.println(album.getArtist());
         System.out.println(author.getLastName());
-        System.out.println(quote.getContent());
+        System.out.println(firstQuote.getContent());
+        System.out.println(firstQuote.getAuthor().getFirstName());
 
+
+     ArrayList<Quote> quotes = new ArrayList<>();
+     quotes.add(firstQuote);
+     quotes.add(secondQuote);
+
+
+     for (Quote q: quotes) {
+      System.out.println(q.getContent());
+      System.out.println(q.getAuthor().getFirstName());
+     }
     }
 
 }
---
+++ src/main/java/DaoFactory.java
@@ -0,0 +1,9 @@
+public class DaoFactory {
+    private static Ads adsDao;
+    public static Ads getAdsDao() {
+        if (adsDao == null) {
+            adsDao = new MySQLAdsDao();
+        }
+        return adsDao;
+    }
+}
