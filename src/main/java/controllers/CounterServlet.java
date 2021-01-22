ba37f0d57f2e3218e1368ddfeef132b043cb0020
Ryan Phillips <ryan.phillips.j92@gmail.com>
2021-01-22 14:53:23 -0600

    mvc exercise

--- src/main/java/controllers/CounterServlet.java
+++ src/main/java/controllers/CounterServlet.java
@@ -1,10 +1,12 @@
+package controllers;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "CounterServlet", urlPatterns = "/counter")
+@WebServlet(name = "controllers.CounterServlet", urlPatterns = "/counter")
 public class CounterServlet extends HttpServlet {
     private int counter = 0;
 
