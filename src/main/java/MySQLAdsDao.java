0c356c1b8d8ccf4f16ee67f68bd6a49cae08c8ab
Ryan Phillips <ryan.phillips.j92@gmail.com>
2021-01-27 12:00:01 -0600

    talked over jdbc exercise

--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,2 +1,65 @@
-package PACKAGE_NAME;public class MySQLAdsDao {
+import com.mysql.cj.jdbc.Driver;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MySQLAdsDao implements Ads {
+
+    private Connection connection;
+
+    public MySQLAdsDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+            this.connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Ad> all() {
+        List<Ad> output = new ArrayList<>();
+        String query = "SELECT * FROM ads";
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+
+            while (rs.next()){
+                output.add(
+                        new Ad(
+                            rs.getLong("id"),
+                            rs.getLong("user_id"),
+                            rs.getString("title"),
+                            rs.getString("description")
+                        )
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return output;
+    }
+
+    @Override
+    public Long insert(Ad ad) {
+        long response = 0;
+        String query = String.format("INSERT INTO ads (id, user_id, title, description) VALUES('%d', '%d', '%s', '%s')", ad.getId(), ad.getUserId(), ad.getTitle(),ad.getDescription());
+//        String query = String.format("INSERT INTO ads (id, user_id, title, description) VALUES('%d', '%d', '%s', '%s')", 1, 1000, "New Title", "Well this is a description" );
+
+        try {
+            Statement statement = connection.createStatement();
+            response = statement.executeUpdate(query);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return response;
+    }
 }
