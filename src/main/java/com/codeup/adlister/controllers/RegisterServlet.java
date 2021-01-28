394527d2baa0f366b8c435d64f463ccef19b6790
Ryan Phillips <ryan.phillips.j92@gmail.com>
2021-01-28 17:18:17 -0600

    working on user input exercise

--- src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
+++ src/main/java/com/codeup/adlister/controllers/RegisterServlet.java
@@ -1,19 +1,56 @@
 package com.codeup.adlister.controllers;
 
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.User;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @WebServlet(name = "controllers.RegisterServlet", urlPatterns = "/register")
 public class RegisterServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         // TODO: show the registration form
+        if (request.getSession().getAttribute("user") != null &&
+                request.getSession().getAttribute("email") != null &&
+                request.getSession().getAttribute("password") != null
+        )  {
+            request.setAttribute("user", request.getSession().getAttribute("user"));
+            request.setAttribute("email", request.getSession().getAttribute("email"));
+            request.setAttribute("password", request.getSession().getAttribute("password"));
+
+            response.sendRedirect("/login");
+            return;
+        }
+        request.getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
         // TODO: ensure the submitted information is valid
         // TODO: create a new user based off of the submitted information
         // TODO: if a user was successfully created, send them to their profile
+
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        String email = request.getParameter("email");
+        boolean checkGood = username.isEmpty() || password.isEmpty() || email.isEmpty();
+        if (!checkGood){
+            User user = new User(username, email, password);
+            Long newUserId = DaoFactory.getUsersDao().insert(user);
+            user.setId(newUserId);
+            request.getSession().setAttribute("user", user);
+            response.sendRedirect("/profile");
+            return;
+        }else {
+            response.sendRedirect("/register");
+        }
+
     }
 }
--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -27,11 +27,24 @@ public class MySQLAdsDao implements Ads {
     }
 
     @Override
+//    public List<Ad> all() {
+//        Statement stmt = null;
+//        try {
+//            stmt = connection.createStatement();
+//            ResultSet rs = stmt.executeQuery("SELECT * FROM ads");
+//            return createAdsFromResults(rs);
+//        } catch (SQLException e) {
+//            throw new RuntimeException("Error retrieving all ads.", e);
+//        }
+//    }
+
     public List<Ad> all() {
-        Statement stmt = null;
+        PreparedStatement stmt = null;
+        String sqlQuery = "SELECT * FROM ads";
         try {
-            stmt = connection.createStatement();
-            ResultSet rs = stmt.executeQuery("SELECT * FROM ads");
+            stmt = connection.prepareStatement(sqlQuery);
+            stmt.executeQuery();
+            ResultSet rs = stmt.getResultSet();
             return createAdsFromResults(rs);
         } catch (SQLException e) {
             throw new RuntimeException("Error retrieving all ads.", e);
@@ -39,15 +52,30 @@ public class MySQLAdsDao implements Ads {
     }
 
     @Override
+//    public Long insert(Ad ad) {
+//        try {
+//            Statement stmt = connection.createStatement();
+//            stmt.executeUpdate(createInsertQuery(ad), Statement.RETURN_GENERATED_KEYS);
+//            ResultSet rs = stmt.getGeneratedKeys();
+//            rs.next();
+//            return rs.getLong(1);
+//        } catch (SQLException e) {
+//            throw new RuntimeException("Error creating a new ad.", e);
+//        }
+//    }
     public Long insert(Ad ad) {
         try {
-            Statement stmt = connection.createStatement();
-            stmt.executeUpdate(createInsertQuery(ad), Statement.RETURN_GENERATED_KEYS);
+        String sql = "Insert INTO Ad(user_id, title, description) VALUES (?,?,?)";
+        PreparedStatement stmt = connection.prepareStatement(createInsertQuery(ad), Statement.RETURN_GENERATED_KEYS);
+
+            stmt.setLong(1, ad.getUserId());
+            stmt.setString(2,ad.getTitle());
+            stmt.setString(3,ad.getDescription());
             ResultSet rs = stmt.getGeneratedKeys();
             rs.next();
             return rs.getLong(1);
         } catch (SQLException e) {
-            throw new RuntimeException("Error creating a new ad.", e);
+            throw new RuntimeException("Error retrieving all ads.", e);
         }
     }
 
